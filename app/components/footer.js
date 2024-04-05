d3b99366e00791a2f7570a6bd3cb70fa8be726ee
mohammad dhikri <mohammad18018@mail.unpad.ac.id>
2024-04-05 21:31:15 +0700

    update footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -18,31 +18,32 @@ export default function Footer() {
             <li>Amazon Science</li>
           </ul>
           <ul>
-            <li><p>Get to Know Us</p></li>
-            <li>Careers</li>
-            <li>Blog</li>
-            <li>About Amazon</li>
-            <li>Investor Relations</li>
-            <li>Amazon Devices</li>
-            <li>Amazon Science</li>
+            <li><p>Make Money with Us</p></li>
+            <li>Sell products on Amazon</li>
+            <li>Sell on Amazon Business</li>
+            <li>Sell apps on Amazon</li>
+            <li>Become an Affiliate</li>
+            <li>Advertise Your Products</li>
+            <li>Self-Publish with Us</li>
+            <li>Host an Amazon Hub</li>
           </ul>
           <ul>
-            <li><p>Get to Know Us</p></li>
-            <li>Careers</li>
-            <li>Blog</li>
-            <li>About Amazon</li>
-            <li>Investor Relations</li>
-            <li>Amazon Devices</li>
-            <li>Amazon Science</li>
+            <li><p>Amazon Payment</p></li>
+            <li>Amazon Business Card</li>
+            <li>Shop with Points</li>
+            <li>Reload Your Balance</li>
+            <li>Amazon Currency Converter</li>
           </ul>
           <ul>
-            <li><p>Get to Know Us</p></li>
-            <li>Careers</li>
-            <li>Blog</li>
-            <li>About Amazon</li>
-            <li>Investor Relations</li>
-            <li>Amazon Devices</li>
-            <li>Amazon Science</li>
+            <li><p>Let Us Help You</p></li>
+            <li>Amazon and COVID-19</li>
+            <li>Your Account</li>
+            <li>Your Orders</li>
+            <li>Shipping Rates & Policies</li>
+            <li>Returns & Replacements</li>
+            <li>Manage Your Content and Devices</li>
+            <li>Amazon Assistant</li>
+            <li>Help</li>
           </ul>
           {/* Repeat for other lists */}
         </div>
